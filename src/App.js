2e3a679029442bbbbeee8a3a4c37a4ddaa8679df
Tom Rozendaal <tom.rozendaal@finalist.nl>
2020-01-17 10:57:21 +0100

    Fetch movie

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Jumbotron } from 'react-bootstrap'
+import { getMovie } from './movie-service'
 import './App.css'
 
 function App(props) {
-  const movie = {
-    id: 1,
-    title: 'One',
-    image:
-      'https://m.media-amazon.com/images/M/MV5BOTdmNTFjNDEtNzg0My00ZjkxLTg1ZDAtZTdkMDc2ZmFiNWQ1XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_SY1000_CR0,0,631,1000_AL_.jpg'
-  }
+  const [movie, setMovie] = useState(null)
+
+  useEffect(() => {
+    getMovie(props.history.match.params.id).then(response => setMovie(response))
+  }, [])
 
   return (
     <Container id="movie">
-      <Jumbotron>
-        <h1>{movie.title}</h1>
-        <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content
-          or information.
-        </p>
-        <iframe
-          className="player"
-          src="https://www.youtube.com/embed/cT9mxZUQ2JM"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
-      </Jumbotron>
+      {movie && (
+        <Jumbotron>
+          <h1>{movie.title}</h1>
+          <p>
+            This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured
+            content or information.
+          </p>
+          <iframe
+            className="player"
+            src={movie.video}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </Jumbotron>
+      )}
     </Container>
   )
 }
